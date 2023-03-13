19f6806f8bfbc0fbefc5bdbada3b9d6b5b006a36
Alexandra Kasianova <gubaydullina.s@mail.ru>
2023-03-13 13:44:46 +0300

    Added homework 2 by decorators and iterators

---
+++ hw2_decorators_and_iterators/code/task5.py
@@ -0,0 +1,62 @@
+from dataclasses import dataclass
+import os
+import re
+
+
+@dataclass
+class FastaRecord:
+    seq: str
+    id_: str
+    description: str
+
+
+class OpenFasta:
+    def __init__(self, file_name):
+        self._fasta_record = FastaRecord(seq=None, id_=None, description=None)
+        self.file_obj = open(file_name)
+        self.__seq_l = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.file_obj.close()
+
+    def __iter__(self):
+        yield self.file_obj
+
+    def read_record(self):
+        counter_name_seq = 0
+        self.__seq_l.clear()
+        while counter_name_seq <= 2:
+            line = next(self.file_obj)
+            line = line.strip('\n')
+            if line.startswith('#'):
+                continue
+
+            elif line.startswith('>'):
+                if self._fasta_record.id_ is None:  # need for first line: >gene spesies
+                    self._fasta_record.id_, self._fasta_record.description = re.findall(r'(.+?) (.+)', line)[0]
+                else:
+                    self._fasta_record.seq = ''.join(self.__seq_l)
+                    # make object with current values
+                    current_fasta_record = self._fasta_record
+                    # clear current info
+                    self._fasta_record = FastaRecord(seq=None, id_=None, description=None)
+                    # remember for next iteration
+                    self._fasta_record.id_, self._fasta_record.description = re.findall(r'(.+?) (.+)', line)[0]
+                    return current_fasta_record
+
+            else:
+                self.__seq_l.append(line)
+
+    def read_records(self):
+        result_all_fasta = []
+        while True:
+            try:
+                result_all_fasta.append(self.read_record())
+            except StopIteration:
+                # write last sequences
+                self._fasta_record.seq = ''.join(self.__seq_l)
+                result_all_fasta.append(self._fasta_record)
+                return result_all_fasta
